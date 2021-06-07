a61d3cf4786d1fb29197194eb905a9e30eb115c4
Mohamad Bagoes Ali Yuddin <72675355+MohamadBagoesAliYuddin@users.noreply.github.com>
2021-06-07 20:48:12 +0700

    Add files via upload

---
+++ PRAKTIKUM 7/Nomor 2.cpp
@@ -0,0 +1,44 @@
+//MOHAMAD BAGOES ALI YUDDIN
+//20051397048
+//MI_B_2020
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+int Data[MAX];
+// Fungsi pengurutan penyisipan langsung
+void StraightInsertSort()
+{
+int i, j, x;
+for(i=1; i<MAX; i++){
+x = Data[i];
+j = i - 1;
+while (x < Data[j]){
+Data[j+1] = Data[j];
+j--;
+}
+Data[j+1] = x;
+}
+}
+int main()
+{
+int i;
+srand(0);
+printf("MOHAMAD BAGOES ALI YUDDIN\n");
+printf("20051397048\n");
+printf("MI_B_2020\n");
+// Membangkitkan bilangan acak
+printf("DATA SEBELUM TERURUT");
+for(i=0; i<MAX; i++)
+{
+Data[i] = (int) rand()/1000+1;
+printf("\nData ke %d : %d ", i, Data[i]);
+}
+StraightInsertSort();
+// Data setelah terurut
+printf("\nDATA SETELAH TERURUT");
+for(i=0; i<MAX; i++)
+{
+printf("\nData ke %d : %d ", i, Data[i]);
+}
+}
